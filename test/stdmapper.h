e810416be4eb8becf9c42fcf85bbe5812264b5e0
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2024-01-25 13:09:26 +0100

    Working

--- test/stdmapper.h
+++ test/stdmapper.h
@@ -3,6 +3,7 @@
 #ifndef STDMAPPER_H
 #define STDMAPPER_H
 
+#include <cerrno>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -13,8 +14,66 @@
 class Stream {
 private:
     FILE *file;
+    char *fname;
+    bool preopened;
+
 public:
-    Stream(FILE *pfile) { file= pfile; }
+    Stream(FILE *pfile, const char *pfname, bool ppreopened) {
+        file= pfile;              /* ownership taken */
+        fname= strdup(pfname);    /* a copy  */
+        preopened= ppreopened;
+    }
+
+    Stream() {
+        file= NULL;
+        fname= NULL;
+        preopened= 0;
+    }
+
+    ~Stream() {
+        if (!preopened && file) {
+            fprintf(stderr, "Stream.destructor *** file '%s' has never been closed\n",
+                    fname? fname: "[noname]");
+            fclose(file);
+            file= NULL;
+        }
+        if (fname) {
+            free(fname);
+            fname= NULL;
+        }
+    }
+
+    size_t readBytes(char *buff, size_t len) {
+        size_t rdlen= fread(buff, 1, len, file);
+        fprintf(stderr, "readBytes(%d) read %d bytes\n", (int)len, (int)rdlen);
+        return rdlen;
+    }
+
+    size_t write(unsigned char *buff, size_t len) {
+        size_t wrlen= fwrite(buff, 1, len, file);
+        fprintf(stderr, "write(%d) has written %d bytes\n", (int)len, (int)wrlen);
+        return wrlen;
+    }
+
+    int close() {
+        if (preopened) {
+            fprintf(stderr, "Stream.close *** don't close file '%s', it is preopened\n",
+                    fname? fname: "[noname]");
+        } else {
+            if (file) {
+                fprintf(stderr, "Stream.closing file '%s'-t\n",
+                        fname? fname: "[noname]");
+                fclose(file);
+                file= NULL;
+            } else {
+                if (fname) {
+                    fprintf(stderr, "Stream.close *** file '%s' has already been closed\n",
+                        fname? fname: "[noname]");
+                }
+            }
+        }
+        return 0;
+    }
 
     void print(const char *s) {
         fputs(s, file);
@@ -36,8 +95,28 @@ public:
 };
 
 class File: public Stream {
+public:
+    File():Stream() {}
+    File(FILE *pfile, const char *pfname, bool ppreopened):
+      Stream(pfile, pfname, ppreopened) {}
 };
 
-Stream Serial(stdout);
+Stream Serial(stdout, "Serial-stdout", true);
+
+class TSPIFFS {
+public:
+    File open(const char *name, const char *mode) {
+        FILE *f= fopen(name, mode);
+        if (f==NULL) {
+            int ern= errno;
+            fprintf(stderr, "Error opening file '%s' mode '%s' errno=%d: %s\n",
+                    name, mode, ern, strerror(ern));
+            exit(1);
+        }
+        return File(f, name, false);
+    }
+};
+
+TSPIFFS SPIFFS;
 
 #endif
--- test/test1.cc
+++ test/test1.cc
@@ -26,4 +26,22 @@ int main(int argc, char **argv) {
 
 static void Test1(const char *fname) {
     fprintf(stderr, "Now trying '%s'\n", fname);
+
+    File f= SPIFFS.open(fname, "r");	// Open source file
 }
+#if 0
+Tar<FS> tar(&SPIFFS);			// Declare and initialize class passing pointer to SPIFFS as target filesystem
+
+void setup() {
+  Serial.begin(74880);
+  SPIFFS.begin();			// Initialize SPIFFS
+  File f = SPIFFS.open(FILENAME, "r");	// Open source file
+  if (f) {
+    tar.open((Stream*)&f);		// Pass source file as Stream to Tar object
+    tar.dest("/");			// Set destination prefix to append to all files in archive. Should start with "/" for SPIFFS
+    tar.extract();			// Process with extraction
+  } else {
+    Serial.println("Error open .tar file");
+  }
+}
+#endif
