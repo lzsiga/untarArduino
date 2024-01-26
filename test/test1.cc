10274af44c6f7138f4284d8b22c5f3b0d9293f2f
Lorinczy Zsigmond <lzsiga@t-online.hu>
2024-01-26 21:08:07 +0100

    options

--- test/test1.cc
+++ test/test1.cc
@@ -5,15 +5,19 @@
 #include "stdmapper.h"
 #include "untar.h"
 
-const char *progname= NULL;
+static struct {
+    const char *progname;
+    const char *prefix;
+} var= {
+    NULL,
+    "./"
+};
 
 static void Test1(const char *fname);
+static void ParseArgs(int *pargc, char ***pargv);
 
 int main(int argc, char **argv) {
-    progname= argv[0];
-
-    Serial.begin(74880);
-    SPIFFS.begin();
+    ParseArgs(&argc, &argv);
 
     if (argc>1) {
         int i;
@@ -22,7 +26,7 @@ int main(int argc, char **argv) {
             Test1(argv[i]);
         }
     } else {
-        fprintf(stderr, "usage: %s <filename> ...\n", progname);
+        fprintf(stderr, "usage: %s <filename> ...\n", var.progname);
     }
     return 0;
 }
@@ -37,7 +41,49 @@ static void Test1(const char *fname) {
         return;
     }
     tar.open(&f);
-    tar.dest("./");
+    tar.dest(var.prefix);
     tar.extract();
     if (f) f.close();
+}
+
+static void ParseArgs (int *pargc, char ***pargv)
+{
+    char *arg;
+    int argc;
+    char **argv;
+
+    argc = *pargc;
+    argv = *pargv;
+
+    var.progname = argv[0];
+
+    while (--argc>0 && **++argv=='-') {
+        arg = argv[0];
+        switch (arg[1]) {
+        case 0: case '-':
+            --argc, ++argv;
+            goto NO_MORE_OPT;
+
+        case 'p': case 'P':
+            if (strcasecmp (argv[0], "-prefix")==0) {
+                if (argc<2) goto OPTNVAL;
+                --argc;
+                ++argv;
+                var.prefix= argv[0][0] ? argv[0]: NULL;
+                break;
+
+            } else goto UNKOPT;
+
+        default:
+UNKOPT:     fprintf (stderr, "Unknown option: '%s'\n", arg);
+            exit (12);
+OPTNVAL:    fprintf (stderr, "No value for option '%s'\n", arg);
+            exit (12);
+        }
+    }
+NO_MORE_OPT:
+    ++argc, --argv;
+
+    *pargc = argc;
+    *pargv = argv;
 }
